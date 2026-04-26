fa599626726f04bc4e499951f52dbd973ffeccf1
Albxrt05 <albertvg747@gmail.com>
2026-04-25 17:04:00 -0700

    Implement dashboard printing for greenhouse monitoring every minute
    Add dashboard printing functionality to monitor greenhouse status.

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,9 @@
 const int LOWER_TEMP = 21;
 const int UPPER_TEMP = 29;
 
+unsigned long lastDashboard = 0;
+const unsigned long DASH_INTERVAL = 60000; // 60,000 ms = 1 minute
+
 
 Adafruit_BME680 bme; // I2C
 //Adafruit_BME680 bme(BME_CS); // hardware SPI
@@ -97,6 +100,9 @@ bool tempInRange(int temp) {
   } else {
     return false;
   }
+  if (millis() - lastDashboard >= DASH_INTERVAL) {
+    printDashboard();
+    lastDashboard = millis();
 }
 
 
@@ -107,4 +113,53 @@ void setupBME() {
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150); // 320*C for 150 ms
+}
+
+void printDashboard() {
+  if (!bme.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    return;
+  }
+
+  float temp = bme.temperature;
+  float humidity = bme.humidity;
+  float gas = bme.gas_resistance / 1000.0;
+
+  // update state based on temp
+  if (!tempInRange(temp)) {
+    greenhouse = WARNING_TEMP;
+  } else {
+    greenhouse = IDLE;
+  }
+
+  Serial.println("----- GREENHOUSE STATUS -----");
+
+  Serial.print("Temp: ");
+  Serial.print(temp);
+  Serial.println(" C");
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  Serial.print("Gas: ");
+  Serial.print(gas);
+  Serial.println(" KOhms");
+
+  Serial.print("Pressure: ");
+  Serial.print(bme.pressure / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Altitude: ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.print("State: ");
+  switch (greenhouse) {
+    case IDLE: Serial.println("IDLE"); break;
+    case WATERING: Serial.println("WATERING"); break;
+    case WARNING_TEMP: Serial.println("WARNING_TEMP"); break;
+  }
+
+  Serial.println("-----------------------------\n");
 }
